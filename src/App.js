2140d6cf00b3c48951660f347ce7050631e68155
SriPrasannanjaneyuluK <anandkalvakolanu@AnandKalvakolanu.localdomain>
2026-05-22 12:09:38 +0000

    Fixed failing test

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hello mister..! 
+          learn react
         </a>
       </header>
     </div>
